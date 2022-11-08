bdb1087677d99647e43d9fcc456b8ee89aaf5384
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-08 18:46:51 -0500

    Write snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const view = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  expect(view).toMatchSnapshot();
 });
